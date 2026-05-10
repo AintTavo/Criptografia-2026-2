487b0989b8fee73104baa63579aa76f9e9b2ab9e
AintTavo <gustavo.25.zgo@gmail.com>
2026-05-09 21:02:35 -0600

    feat and docs: PCBC added / Commenting of OFB and CFB

--- Extras/modus_openrandi/src/main.rs
+++ Extras/modus_openrandi/src/main.rs
@@ -38,6 +38,9 @@ fn main() {
     let cipher_text_ofb = modus_ofb_hc(&mesage, 3, &init, &key, m);
     debug_block("cipher Text OFB", &cipher_text_ofb);
 
+    let cipher_text_cbc = modus_pcbc_hc(&mesage, 3, &init, &key, m);
+    debug_block("cipher Text PCBC", &cipher_text_cbc);
+
     let tmp_block = block_xor(&block, &block_2);
     debug_block("Xor", &tmp_block);
 
@@ -200,131 +203,198 @@ pub fn modus_cbc_hc(
 
 
 // -> Modo de operación Cipher Feedback (CFB) : 
+// Cn = Ek(Cn-1) xor p 
 pub fn modus_cfb_hc(
-    msg : &[i32], 
-    block_size : usize, 
-    c_0 :&[i32], 
-    key : &[i32], 
-    m : i32 
+    msg : &[i32],       // Mensaje a encriptar
+    block_size : usize, // Tamaño de bloque
+    c_0 :&[i32],        // Vector de inicialización (IV)
+    key : &[i32],       // Llave
+    m : i32             // Modulo
 ) -> Vec<i32> {
-    let mut cipher_text: Vec<i32> = Vec::new();
+    let mut cipher_text: Vec<i32> = Vec::new(); // Variable de retorno
 
+    // # Corrección de errores CFB(1):
+    // Verificación de integridad de la llave para el cifrado Hill
     if key.len() != (block_size * block_size) {
         error("The key does not correspond with the block size for hill cipher");
         cipher_text.push(1);
         return cipher_text;
     }
 
+    // # Corrección de errores CFB(2):
+    // Verificación de que el IV tenga el tamaño de bloque correspondiente
     if c_0.len() != block_size {
         error("The initial block is not the size of a normal block");
         cipher_text.push(1);
         return cipher_text;
     } 
 
-    let mut _tmp_msg: Vec<i32> = Vec::new();
-    let padding = block_size - (msg.len() % block_size);
+    let mut _tmp_msg: Vec<i32> = Vec::new();                            // Variable para reajustar el mensaje
+    let padding = ( block_size - ( msg.len() % block_size ) ) % block_size;                // Cálculo del relleno necesario
 
-    _tmp_msg = msg.to_vec().clone();
+    _tmp_msg = msg.to_vec().clone();                                    // Clonación del mensaje original
 
-    if msg.len() % block_size != 0 {
-        for _ in 0..padding {
-            _tmp_msg.push(1);
-        }
-    }
+    // # Corrección de errores CFB(3):
+    // Relleno de bloques incompletos con 1's si no es múltiplo del tamaño de bloque
+    _tmp_msg.extend(repeat(1).take(padding));
 
+    // División del mensaje en bloques del tamaño especificado
     let msg_blocks = _tmp_msg.chunks(block_size);
-    let mut tmp_block : Vec<i32> = c_0.to_vec().clone();
+    let mut tmp_block : Vec<i32> = c_0.to_vec().clone();                // Registro temporal para encadenamiento
 
+    // # Ciclo : Main Loop de Cipher Feedback
     for i in msg_blocks {
         let block = i.to_vec();
 
-        let _cipher_block = hill_cipher(&tmp_block, &key, m);
-        let _xor_result = block_xor(&block, &_cipher_block);
-        tmp_block = _xor_result.to_vec().clone();
+        let _c_ek = hill_cipher(&tmp_block, &key, m);                   // Se cifra el bloque anterior o IV
+        let _cipher_block = block_xor(&block, &_c_ek);                  // XOR entre texto plano y salida del cifrador
+        tmp_block = _cipher_block.to_vec().clone();                     // La salida cifrada alimenta el siguiente bloque
 
-        cipher_text.extend(_xor_result);
+        cipher_text.extend(_cipher_block);                              // Se anexa al resultado final
     }
 
-    
-    cipher_text.drain(( cipher_text.len() - padding )..cipher_text.len());
+    // Eliminación de los datos de holgura (padding) del mensaje final
+    let _cipher_size = cipher_text.len();                           // Se calcula el tamaño del mensaje final
+    cipher_text.drain(( _cipher_size - padding ).._cipher_size);    // Se le recortan los datos de holgura
 
     return cipher_text;
 }
 
 
-// -> Modo de operación Cipher Feedback (OFB) :
+// -> Modo de operación Output Feedback (OFB) :
+// C = Ek(Co) xor p
 pub fn modus_ofb_hc(
-    msg : &[i32], 
-    block_size : usize, 
-    c_0 :&[i32], 
-    key : &[i32], 
-    m : i32 
+    msg : &[i32],       // Mensaje a encriptar
+    block_size : usize, // Tamaño de bloque
+    c_0 :&[i32],        // Vector de inicialización (IV)
+    key : &[i32],       // Llave
+    m : i32             // Modulo
 ) -> Vec<i32> {
-    let mut cipher_text: Vec<i32> = Vec::new();
+    let mut cipher_text: Vec<i32> = Vec::new(); // Variable de retorno
 
+    // # Corrección de errores OFB(1):
+    // Verificación de tamaño de llave
     if key.len() != (block_size * block_size) {
         error("The key does not correspond with the block size for hill cipher");
         cipher_text.push(1);
         return cipher_text;
     }
 
+    // # Corrección de errores OFB(2):
+    // Verificación de tamaño del bloque inicial
     if c_0.len() != block_size {
         error("The initial block is not the size of a normal block");
         cipher_text.push(1);
         return cipher_text;
     } 
 
-    let mut _tmp_msg: Vec<i32> = Vec::new();
-    let padding = block_size - (msg.len() % block_size);
+    let mut _tmp_msg: Vec<i32> = Vec::new();                            // Variable auxiliar para el mensaje
+    let padding = ( block_size - ( msg.len() % block_size ) ) % block_size;                // Variable de ajuste de tamaño
 
     _tmp_msg = msg.to_vec().clone();
 
-    if msg.len() % block_size != 0 {
-        for _ in 0..padding {
-            _tmp_msg.push(1);
-        }
-    }
+    // Relleno manual de bloques si el mensaje no es múltiplo del bloque
+    _tmp_msg.extend(repeat(1).take(padding));
 
     let msg_blocks = _tmp_msg.chunks(block_size);
-    let mut tmp_block : Vec<i32> = c_0.to_vec().clone();
+    let mut tmp_block : Vec<i32> = c_0.to_vec().clone();                // Bloque para retroalimentación de salida
 
+    // # Ciclo : Main Loop de Output Feedback
     for i in msg_blocks {
         let block = i.to_vec();
 
-        let _c_0_ek = hill_cipher(&tmp_block, &key, m);
-        let _cipher_block = block_xor(&_c_0_ek, &block);
+        let _c_0_ek = hill_cipher(&tmp_block, &key, m);                 // Se cifra el flujo de salida independientemente del mensaje
+        let _cipher_block = block_xor(&_c_0_ek, &block);                // XOR entre el flujo cifrado y el texto plano
         
-        tmp_block = _c_0_ek.to_vec().clone();
+        tmp_block = _c_0_ek.clone();                                    // La salida del cifrador (antes de XOR) se retroalimenta
 
         cipher_text.extend(_cipher_block);
     }
 
-    
-    cipher_text.drain(( cipher_text.len() - padding )..cipher_text.len());
+    // Recorte de padding para recuperar tamaño original
+    let _cipher_size = cipher_text.len();                           // Se calcula el tamaño del mensaje final
+    cipher_text.drain(( _cipher_size - padding ).._cipher_size);    // Se le recortan los datos de holgura
 
     return cipher_text;
 }
 
-pub fn modus_pcbc_hc() {
+// -> Modo de operación Propagating Cipher Block Chaining (PCBC)
+// n = 0    |   C = Ek ( p xor Co )
+// n = 1    |   C = Ek ( (p n-1 xor C n-1) xor p )
+pub fn modus_pcbc_hc(
+    msg : &[i32],       // Mensaje a encriptar
+    block_size : usize, // Tamaño de bloque
+    c_0 :&[i32],        // Vector de inicialización (IV)
+    key : &[i32],       // Llave
+    m : i32             // Módulo
+) -> Vec<i32> {
+    let mut cipher_text: Vec<i32> = Vec::new();     // Varaible de retorno
 
+    // Correción de errores CBC (1):
+    // Si la llave para el cifrado hill no es el cuadrado del tamaño del bloque retorna 1.
+    if key.len() != (block_size * block_size) {
+        error("The key does not correspond with the block size for hill cipher");
+        cipher_text.push(1);
+        return cipher_text;
+    }
+
+    // Correción de errores CBC (2):
+    // Si el bloque de cifrado inicial no corresponde con el tamaño correcto retorna 1.
+    if c_0.len() != block_size {
+        error("The initial block is not the size of a normal block");
+        cipher_text.push(1);
+        return cipher_text;
+    } 
+
+    let mut _tmp_msg: Vec<i32> = Vec::new();                               // Variable para copiar y reajustar el mensaje a cifrar
+    let padding = (block_size - (msg.len() % block_size)) % block_size;    // Variable para determinar si el ultimo bloque tiene suficiente tamaño
+
+    _tmp_msg = msg.to_vec().clone();        // Se clona el mensaje original
+
+    // # Correción de errores CBC (2):
+    // En caso de que el bloque final no tenga suficiente tamaño, se le añaden 1's para rellenarlo
+    _tmp_msg.extend(repeat(1).take(padding));
+
+    // Se rompe el mensaje en un arreglo de arreglos del tamaño del bloque
+    let msg_blocks = _tmp_msg.chunks(block_size);
+
+    let mut tmp_block : Vec<i32> = c_0.to_vec(); // Se inicializa el valor del bloque temporal con el del bloque inicial
+
+    // # Ciclo : Main Loop de Propagating Cipher Block Chaining 
+    for i in msg_blocks {
+        let block = i.to_vec();                                 // i es un apuntador entonce se convierte a un vector
+
+        let _xor_result = block_xor(&block, &tmp_block);        // Se aplica una xor con el bloque anterior 
+        let _cipher_block = hill_cipher(&_xor_result, &key, m); // Se aplica el cifrado al bloque
+        tmp_block = block_xor(&block, &_cipher_block);          // Se aplica la funcion de propagación
+
+        cipher_text.extend(_cipher_block);                      // Se guarda el bloque cifrado en el texto final
+    }
+
+    
+    let _cipher_size = cipher_text.len();                           // Se calcula el tamaño del mensaje final
+    cipher_text.drain(( _cipher_size - padding ).._cipher_size);    // Se le recortan los datos de holgura
+
+    return cipher_text;
 }
+
 
 pub fn modus_ctr_hc() {
-
+    // Pendiente de implementación
 }
 
-/*  
-    --------------------------------------------------------------------------------
+/* --------------------------------------------------------------------------------
     Tool functions : Funciones que sirven principalmente como herramientas para el resto del desarrollo.
     --------------------------------------------------------------------------------
 */  
 
 // -> Función Modulo, pero siempre positivo
-#[inline(always)]   // Hace que en donde este en el codigo el compilador sustituya esa parte de codigo por directamente la función
+#[inline(always)]   // Sugerencia al compilador para expansión inline
 fn module( a: i32 , m : i32 ) -> i32 {
-    return ( ( a % m ) + m ) % m ;  // Al sumar un modulo y aplicarlo nuevamente y darle modulo hace que sean siempre positivos.
+    return ( ( a % m ) + m ) % m ;  // Garantiza que el resultado esté en el rango [0, m-1]
 }
 
+// -> Función Block XOR : Realiza una operación XOR bit a bit entre dos vectores
 fn block_xor( block_1 : &[i32], block_2 : &[i32]) -> Vec<i32> {
     let  result : Vec<i32> = block_1.to_vec().iter()
         .zip(block_2.to_vec().iter())
@@ -338,7 +408,7 @@ fn error( message : &str ) {
     println!("{} {}", "Error:".red().bold(), message.red());
 } 
 
-
+// -> Función Debug Block : Imprime en consola un bloque con formato de depuración
 fn debug_block(label : &str, block : &[i32]) {
     print!("{}", "Debug [".yellow().bold());
     print!("{}", label.yellow().italic());
