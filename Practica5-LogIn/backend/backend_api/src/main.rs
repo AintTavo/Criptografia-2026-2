75cab87b7aff8caae51c436c700cc192b8c23ac7
AintTavo <gustavo.25.zgo@gmail.com>
2026-05-14 19:35:28 -0600

    init: Adding initial crates to the api

--- Practica5-LogIn/backend/backend_api/src/main.rs
+++ Practica5-LogIn/backend/backend_api/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
+// Librearías estandar
+
+
+// Crates Externas
+// API
+use axum::{routing::{get,post,patch}, Json, Router};
+use serde::{Deserialize, Serialize};
+
+// SQL Connection
+use rusqlite::{Connection, Result};
+
+// Hashing
+use sha3::Sha3_256;
+
+// Terminal Colors
+use colored::Colorize;
+
+#[tokio::main]
+async fn main() {
     println!("Hello, world!");
 }
