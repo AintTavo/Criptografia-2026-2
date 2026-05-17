5dd7be796f9305475188c36dcce74e82782aaa22
AintTavo <gustavo.25.zgo@gmail.com>
2026-05-17 11:07:14 -0600

    feat: Restore mail implemented

--- Practica5-LogIn/backend/backend_api/src/main.rs
+++ Practica5-LogIn/backend/backend_api/src/main.rs
@@ -10,7 +10,7 @@ use serde::{Deserialize, Serialize};
 use serde_json::json;
 
 // SQL Connection
-use rusqlite::{params, Connection, Result};
+use rusqlite::{Connection, Result, params};
 
 // Hashing
 use sha3::{Digest, Sha3_256};
@@ -21,6 +21,12 @@ use colored::Colorize;
 // JWT Token management
 use jsonwebtoken::{encode, EncodingKey, decode, DecodingKey, Header, TokenData, Validation, errors::Error };
 use chrono::{Utc, Duration};
+
+// Lettre - Emails through rust
+use lettre::transport::smtp::authentication::Credentials;
+use lettre::{Message, AsyncSmtpTransport, AsyncTransport, Tokio1Executor};
+
+use url::Url;
 
 
 // -> Structs for requests
@@ -38,9 +44,20 @@ struct LogInRequest {
     password : String,
 }
 
+
 #[derive(Deserialize, Serialize, Debug)]
 struct ValRequest {
     jwt : String,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+struct RestoreInitRequest {
+    email : String,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+struct RestoreFinalRequest {
+    token : String,
 }
 
 
@@ -51,6 +68,14 @@ pub struct Claims {
     pub sub: String,    // ID del usuario (Subject)
     pub exp: usize,     // Tiempo de expiración (Timestamp)
     pub iat: usize,     // Tiempo de creación
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct RestoreClaims {
+    pub email : i32,
+    pub pass : Vec<u8>,
+    pub exp : usize,
+    pub iat : usize,
 }
 
 /*  
@@ -70,7 +95,7 @@ async fn main() {
 
     let exist = Path::new(db_path).exists();
     
-    println!("{} Listening on port 3000", "[Server] :".green().bold());
+    println!("{} Initializing db", "[Server] :".green().bold());
     let conn = Connection::open(db_path).unwrap_or_else(
         |_| panic!("Failed to open database.")
     );
@@ -93,13 +118,15 @@ async fn main() {
         .route("/login", get(handle_request_login))
         .route("/val", get(handle_request_val))
         .route("/signin", post(handle_request_signin))
-        .route("/restore", get(handle_request_restore_get))
+        .route("/restore/init", post(handle_request_restore_init))
         .route("/restore", post(handle_request_restore_post));
 
     println!("{} API function created","[Server] :".green().bold());
-    
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    println!("{} Listening on port 3000", "[Server] :".green().bold());
+
+    let port = env::var("PORT").expect("PORT not declared in de .env");
+    let api_ip = format!("0.0.0.0:{}",port);
+    let listener = tokio::net::TcpListener::bind(&api_ip).await.unwrap();
+    println!("{} Listening on port {}", "[Server] :".green().bold(), port);
 
     
     println!("{}", "Server started successfully".green());
@@ -118,7 +145,8 @@ async fn main() {
 // Saves the user's credentials and creates a session token
 async fn handle_request_signin(Json(payload) : Json<SignInRequest>) -> impl IntoResponse {
     println!("{} Processing sign in request", "[Signin/POST] :".yellow());
-    let conn = Connection::open("sqlite.db").unwrap_or_else(|_| panic!("Connection failed"));
+    let db_file = env::var("DATABASE_FILE").expect("Error: DATABASE_FILE not defined in .env");
+    let conn = Connection::open(db_file).unwrap_or_else(|_| panic!("Connection failed"));
     println!("{} Conexion opened with SQLite db", "[Signin/POST] :".yellow());
 
     let mut hasher = Sha3_256::new();
@@ -139,14 +167,14 @@ async fn handle_request_signin(Json(payload) : Json<SignInRequest>) -> impl Into
 
             match jwt {
                 Ok(token_string) => {
-                    println!("{} JWT Created Succesfully", "[Login/GET] :".blue());
+                    println!("{} JWT Created Succesfully", "[Signin/POST] :".yellow());
                     return (StatusCode::OK, Json(json!({
                         "status" : "Success",
                         "jwt" : token_string
                     })));
                 },
                 Err(_) => {
-                    println!("{} JWT was not created", "[Login/GET] :".blue());
+                    println!("{} JWT was not created", "[Signin/POST] :".yellow());
                     error_response = "Error creating the JWT".to_string();
                 }
             }
@@ -169,7 +197,8 @@ async fn handle_request_signin(Json(payload) : Json<SignInRequest>) -> impl Into
 // -> Request for log in
 async fn handle_request_login(Json(payload) : Json<LogInRequest>) -> impl IntoResponse {
     println!("{} Processing log in request", "[Login/GET] :".blue());
-    let conn = Connection::open("sqlite.db").unwrap_or_else(|_| panic!("Connection failed"));
+    let db_file = env::var("DATABASE_FILE").expect("Error: DATABASE_FILE not defined in .env");
+    let conn = Connection::open(db_file).unwrap_or_else(|_| panic!("Connection failed"));
     println!("{} Connection opened with SQLite db", "[Login/GET] :".blue());
 
     let mut stmt = conn.prepare("SELECT email, password FROM users WHERE email = ?1").unwrap_or_else(|_| panic!("Error building the query"));
@@ -245,8 +274,19 @@ async fn handle_request_val(Json(payload) : Json<ValRequest>) -> impl IntoRespon
     let val_result = validate_jwt(&token, &secret);
     
     match val_result {
-        Ok(_) => {
+        Ok(token_data) => {
             println!("{} The JWT was correct", "[Val/GET] :".purple());
+
+            if token_data.claims.exp == Utc::now().timestamp() as usize {
+                return (
+                    StatusCode::OK,
+                    Json(json!({
+                        "status" : "success",
+                        "val" : false
+                    }))
+                );
+            }
+            
             return (
                 StatusCode::OK,
                 Json(json!({
@@ -265,6 +305,7 @@ async fn handle_request_val(Json(payload) : Json<ValRequest>) -> impl IntoRespon
         StatusCode::UNAUTHORIZED,
         Json(json!({
             "status" : "failed",
+            "val" : false,
             "err_msg" : error_response
         }))
     );
@@ -274,8 +315,150 @@ async fn handle_request_val(Json(payload) : Json<ValRequest>) -> impl IntoRespon
 // -> Request for restore init
 // When created take the user's email and send a recovery code to it
 // And changes the token status to "pending"
-async fn handle_request_restore_get() {
-    println!("{} Processing restore init request", "[Restore/GET] :".red());
+// 1. Cambiamos 'impl IntoResponse' por el tipo exacto de la tupla
+async fn handle_request_restore_init(
+    Json(payload): Json<RestoreInitRequest>,
+) -> (StatusCode, Json<serde_json::Value>) {
+    
+    println!("{} Processing restore init request", "[Restore/POST] :".red());
+    let error_val : u8;
+    let error_response : String;
+
+    let db_file = env::var("DATABASE_FILE").expect("Error: DATABASE_FILE not defined in .env");
+    let unique_jwt : Option<String>;
+    let user_id : i32;
+    let username : String;
+    let restore_url : String;
+    
+    {
+        let conn = Connection::open(db_file).unwrap_or_else(|_| panic!("Connection failed"));
+        let mut stmt = conn.prepare("SELECT id,username,password FROM users WHERE email = ?1").unwrap_or_else(|_| panic!("Error building the query"));
+        let mut results = stmt.query(params![payload.email]).unwrap();
+        
+    
+        println!("{} Gathering the data to create the restore token", "[Restore/GET] :".red());
+        match results.next() {
+            Ok(Some(row)) => { 
+                user_id = row.get(0).unwrap();
+                username = row.get(1).unwrap();
+                let pass : Vec<u8> = row.get(2).unwrap();
+                let secret = env::var("JWT_SECRET").expect("JWT_SECRET is not defined in .env");
+                let jwt = generate_jwt_for_restore(&user_id, &pass, &secret).unwrap();
+                unique_jwt = Some(jwt);
+                println!("{} Restore token created successfully", "[Restore/GET] :".red());
+            },
+            Ok(None) => {
+                error_val = 1;
+                error_response = "The email is not registered".to_string();
+                println!("{} {}", "[Restore/GET] :".red(), "User emails is not in the database".red());
+                return (
+                    StatusCode::UNAUTHORIZED,
+                    Json(json!({
+                        "status" : "failed",
+                        "err_val" : error_val,
+                        "err_msg" : error_response,
+                        "val" : false
+                    }))
+                );
+            },
+            Err(_) => {
+                error_val = 2;
+                error_response = "Error with the database".to_string();
+                println!("{} {}", "[Restore/GET] :".red(), "Database error".red());
+                return (
+                    StatusCode::UNAUTHORIZED,
+                    Json(json!({
+                        "status" : "failed",
+                        "err_val" : error_val,
+                        "err_msg" : error_response,
+                        "val" : false
+                    }))
+                );
+            }
+        }
+    
+        drop(results);
+        drop(stmt);
+    
+        match unique_jwt {
+            Some(jwt) => {
+                let result = conn.execute(
+                    "INSERT INTO restore_token (user_id, token, timestamp, status) VALUES (?1, ?2, ?3, ?4)", 
+                    params![user_id, jwt, Utc::now().date_naive().to_string(), "pending"]
+                );
+                println!("{} Adding token to db", "[Restore/GET] :".red());
+                match result {
+                    Ok(_) =>{
+                        println!("{}  Token successfully added to db", "[Restore/GET] :".red());
+                        let url_app = env::var("APP_URL").expect("APP_URL not defined in .env");
+                        restore_url = format!("{}/restore?token={}", url_app, jwt);
+                        println!("{} URL for restoring data successfully created", "[Restore/GET] :".red());
+                    },
+                    Err(_) => {
+                        error_val = 4;
+                        error_response = "The token has not been added to the db".to_string();
+                        println!("{} {}", "[Restore/GET] :".red(), "Token was not added to db".red());
+                        return (
+                          StatusCode::UNAUTHORIZED,
+                          Json(json!({
+                              "status" : "failed",
+                              "err_val" : error_val,
+                              "err_msg" : error_response,
+                              "val" : false
+                          }))
+                        );
+                    }
+                }
+            },
+            None => {
+                error_val = 3;
+                error_response = "Restore token was not created".to_string();
+                println!("{} {}", "[Restore/GET] :".red(), "JWT was not created in previous steps".red());
+                return (
+                  StatusCode::UNAUTHORIZED,
+                  Json(json!({
+                      "status" : "failed",
+                      "err_val" : error_val,
+                      "err_msg" : error_response,
+                      "val" : false
+                  }))
+                );
+            }
+        }
+    }
+    
+    
+    println!("{} Sending email ...", "[Restore/GET] :".red());
+    match send_email(&payload.email, &username, &restore_url).await {
+        Ok(_) => {
+            println!("{} Email sended succesfully", "[Restore/GET] :".red());
+        },
+        Err(_) => {
+            error_val = 5;
+            error_response = "An error ocurred while sending the email".to_string();
+            println!("{} {}", "[Restore/GET] :".red(), "Email was not send".red());
+            return (
+              StatusCode::UNAUTHORIZED,
+              Json(json!({
+                  "status" : "failed",
+                  "err_val" : error_val,
+                  "err_msg" : error_response,
+                  "val" : false
+              }))
+            );
+        }
+    }
+    
+    // Eliminamos el 'return' explícito innecesario aquí para seguir el estilo idiomático de Rust
+    return (
+        StatusCode::OK,
+        Json(json!({
+            "status" : "sucess",
+            "err_val" : 0,
+            "err_msg" : "",
+            "val" : true
+        }))
+    )
 }
 
 // -> Request for restore
@@ -284,6 +467,45 @@ async fn handle_request_restore_get() {
 // and restore the password
 async fn handle_request_restore_post() {
     println!("{} Processing restore request", "[Restore/POST] :".red());
+}
+
+
+pub async fn send_email(
+    email : &str,
+    name : &str,
+    url : &str
+) -> Result<(), Box<dyn std::error::Error>> {
+    let url = Url::parse(url).expect("URL in existente");
+
+    let smtp_server = env::var("SMTP_SERVER").expect("SMTP_SERVER not defined in .env");
+    let smtp_user = env::var("SMTP_USER").expect("SMTP_USER not defined in .env");
+    let smtp_pass = env::var("SMTP_PASS").expect("SMTP_PASS not defined in .env");
+
+    let remitent = format!("Example API of login and restore <{}>", smtp_user);
+    let destinatary = format!("{} <{}>", name, email);
+
+    let html_corpse = format!("<p>Hola {},</p>\
+             <p>Por favor, haz clic en el siguiente enlace para verificar tu cuenta:</p>\
+             <p><a href='{}' style='padding: 10px 20px; background-color: #4CAF50; color: white; text-decoration: none; border-radius: 5px;'>Verificar mi cuenta</a></p>\
+             <p>Si el botón no funciona, copia y pega esto en tu navegador: {}</p>
+             <br><p><strong>En caso de que no lo hayas solicitado ignora este mensaje</strong></p>", name, url, url);
+
+    let email = Message::builder()
+        .from(remitent.parse()?)
+        .to(destinatary.parse()?)
+        .subject("Recuperación de contraseña")
+        .header(lettre::message::header::ContentType::TEXT_HTML)
+        .body(html_corpse)?;
+    
+    let creds = Credentials::new(smtp_user,smtp_pass);
+
+    let mailer = AsyncSmtpTransport::<Tokio1Executor>::starttls_relay(&smtp_server)?
+        .credentials(creds)
+        .build();
+
+    mailer.send(email).await?;
+    
+    Ok(())
 }
 
 // -> Generation of JWT
@@ -306,6 +528,27 @@ pub fn generate_jwt(user_id: &str, secret: &str) -> Result<String, Error> {
     )
 }
 
+
+pub fn generate_jwt_for_restore(user_id: &i32, user_secret: &[u8], secret: &str) -> Result<String, Error> {
+    let expiration = Utc::now()
+        .checked_add_signed(Duration::hours(4))
+        .expect("valid timestamp")
+        .timestamp();
+
+    let claims = RestoreClaims {
+        email: user_id.to_owned(),
+        pass: user_secret.to_owned(),
+        iat: Utc::now().timestamp() as usize,
+        exp: expiration as usize,
+    };
+
+    encode(
+        &Header::default(),
+        &claims,
+        &EncodingKey::from_secret(secret.as_bytes()),
+    )
+}
+
 // Función para validar el JWT
 pub fn validate_jwt(token: &str, secret: &str) -> Result<TokenData<Claims>, Error> {
     let validation = Validation::default();
@@ -317,4 +560,7 @@ pub fn validate_jwt(token: &str, secret: &str) -> Result<TokenData<Claims>, Erro
         &validation,
     )
 }
+
+pub async fn email_send(){}
+
 
